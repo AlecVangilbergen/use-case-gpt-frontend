098e95e9351fe0d274eed64712bb4c23c41cfac1
AlecVangilbergen <alec.vangilbergen@student.ucll.be>
2024-09-07 21:31:44 +0200

    Styling update Homepage; Navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -129,32 +129,32 @@ const Navbar: React.FC = () => {
         </div>
       </div>
       <div className="md:block hidden mb-2">
-          {isLoggedIn && (
-            <>
-              <div tabIndex={0} role="button" className={` btn ${getLinkClass('/chat')} rounded-btn`}>
-                <Link to="/chat">Chat</Link>
-              </div>
+        {isLoggedIn && (
+          <>
+            <div tabIndex={0} role="button" className={` btn ${getLinkClass('/chat')} rounded-btn mt-1 font-bold dark:text-dark-text`}>
+              <Link to="/chat">Chat</Link>
+            </div>
 
-              <div tabIndex={0} role="button" className={` btn ${getLinkClass('/document-viewer')} rounded-btn`}>
-                <Link to="/document-viewer">Documents</Link>
-              </div>
+            <div tabIndex={0} role="button" className={` btn ${getLinkClass('/document-viewer')} rounded-btn mt-1 font-bold dark:text-dark-text`}>
+              <Link to="/document-viewer">Documents</Link>
+            </div>
 
-              <div tabIndex={0} role="button" className={` btn ${getLinkClass('/upload-document')} rounded-btn`}>
-                <Link to="/upload-document">Upload Documents</Link>
-              </div>
+            <div tabIndex={0} role="button" className={` btn ${getLinkClass('/upload-document')} rounded-btn mt-1 font-bold dark:text-dark-text`}>
+              <Link to="/upload-document">Upload Documents</Link>
+            </div>
 
-              <div tabIndex={0} role="button" className={` btn ${getLinkClass('/users')} rounded-btn`}>
-                <Link to="/users">Users</Link>
-              </div>
-            </>
-          )}
-          {!isLoggedIn && (
-            <div tabIndex={0} role="button" className={` btn ${getLinkClass('/register')} rounded-btn`}>
+            <div tabIndex={0} role="button" className={` btn ${getLinkClass('/users')} rounded-btn mt-1 font-bold dark:text-dark-text`}>
+              <Link to="/users">Users</Link>
+            </div>
+          </>
+        )}
+        {!isLoggedIn && (
+          <div tabIndex={0} role="button" className={` btn ${getLinkClass('/register')} rounded-btn mt-1 font-bold dark:text-dark-text`}>
             <Link to="/register">Add User</Link>
           </div>)
-          }
-          
-        </div>
+        }
+
+      </div>
       <div>
         <div className="mr-3 mb-1">
           {!isLoggedIn && (
@@ -171,7 +171,7 @@ const Navbar: React.FC = () => {
 
 
       <div className="flex-none">
-        <label className="flex cursor-pointer gap-2 mr-10" htmlFor="theme-switch">
+        <label className="flex cursor-pointer gap-2 mr-5" htmlFor="theme-switch">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -209,6 +209,8 @@ const Navbar: React.FC = () => {
             <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
           </svg>
         </label>
+        <a href="https://github.com/AlecVangilbergen/use-case-gpt-frontend" className="mr-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+        <span className="sr-only">Use-Case-GTP on GitHub</span><svg viewBox="0 0 16 16" className="w-5 h-5" fill="currentColor" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg></a>
       </div>
     </nav>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,40 +14,56 @@ const Home: React.FC = () => {
           <p className="text-xl md:text-2xl mb-12">
             Use-Case-GPT is a powerful tool that helps you generate chat responses in seconds.
           </p>
-          <Button color="white" className="text-indigo-600" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+          <Button color="white" className="text-indigo-600" placeholder="" onPointerEnterCapture={() => { }} onPointerLeaveCapture={() => { }}>
             Get Started
           </Button>
         </div>
       </div>
-
-      {/* Feature Section */}
-      <section className="py-20 bg-white dark:bg-dark-neutral">
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 dark:text-dark-text">
-            Why Use-Case-GPT?
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="p-6 bg-gray-100 rounded-lg shadow-lg dark:bg-dark-neutral">
-              <h3 className="text-xl text-black font-semibold mb-4 dark:text-dark-text">Customizable Templates</h3>
-              <p className="text-black dark:text-dark-text">
-                Personalize your GPT chats with custom templates and formatting options.
-              </p>
-            </div>
-            <div className="p-6 bg-gray-100 rounded-lg shadow-lg dark:bg-dark-neutral">
-              <h3 className="text-xl text-black font-semibold mb-4 dark:text-dark-text">Seamless Integration</h3>
-              <p className="text-black dark:text-dark-text">
-                Integrates smoothly with any development environment.
-              </p>
-            </div>
-            <div className="p-6 bg-gray-100 rounded-lg shadow-lg dark:bg-dark-neutral">
-              <h3 className="text-xl text-black font-semibold mb-4 dark:text-dark-text">User-Friendly Interface</h3>
-              <p className="text-black dark:text-dark-text">
-                Easy-to-use interface to make your work faster and more efficient.
-              </p>
-            </div>
-          </div>
+ {/* Feature Section */}
+<section className="py-20 bg-white dark:bg-dark-neutral">
+  <div className="container mx-auto text-center">
+    <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 dark:text-dark-text">
+      Why Use-Case-GPT?
+    </h2>
+    <div className="space-y-24">
+      <div className="p-6 bg-gray-50 rounded-lg shadow-lg dark:bg-dark-neutral flex items-center justify-between text-left">
+        <div>
+          <h3 className="text-4xl text-black font-semibold mb-4 dark:text-dark-text">Personalized GPT at your disposal</h3>
+          <p className="text-black dark:text-dark-text">
+            Use-Case-GPT is an easy-to-use tool that allows your ChatGPT chats to be tailored to your needs. Enter your own system prompts and upload your own files and let Use-Case-GPT do the rest.
+          </p>
         </div>
-      </section>
+        <img src="src\imgs\1692605748469.jpeg" alt="Customizable Templates" className="w-1/3 ml-4 rounded" />
+      </div>
+      <div className="p-6 bg-gray-50 rounded-lg shadow-lg dark:bg-dark-neutral flex items-center justify-between text-right">
+      <img src="src\imgs\github-cover.jpg" alt="Seamless Integration" className="w-1/3 ml-4 rounded" />
+        <div>
+          <h3 className="text-4xl text-black font-semibold mb-4 dark:text-dark-text">Easy Installation</h3>
+          <p className="text-black dark:text-dark-text">
+            Easily installed locally with minimal setup required. 
+          </p>
+          <p className="text-black dark:text-dark-text" >
+          The process takes no longer than 5 minutes and you're ready to go.
+          </p>
+          <p className="text-black dark:text-dark-text">
+          The installation guide is available on the <a className="link link-primary" href ="https://github.com/AlecVangilbergen/use-case-gpt-frontend">GitHub repository</a>.
+          </p>
+        </div>
+      </div>
+      <div className="p-6 bg-gray-50 rounded-lg shadow-lg dark:bg-dark-neutral flex items-center justify-between text-left">
+        <div>
+          <h3 className="text-4xl text-black font-semibold mb-4 dark:text-dark-text">User-Friendly Interface</h3>
+          <p className="text-black dark:text-dark-text">
+            Easy-to-use interface to make your work faster and more efficient.
+          </p>
+        </div>
+        <img src="src\imgs\663d5ae73b4f55bf91555fdd_388_DesignBlogHeader_2400x1260.jpg" alt="User-Friendly Interface" className="w-1/3 ml-4 rounded" />
+      </div>
+    </div>
+  </div>
+</section>
+
+
 
       <ToastContainer position="top-center" />
 
